7e2bfdd5783f0a9bfcb496f0bb688ea52c835899
“yangquanhui” <1129614034@qq.com>
2018-11-27 14:11:41 +0800

    first commit

---
+++ myansible/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from myansible import views
+
+app_name = 'ansible'
+
+urlpatterns = [
+    url(r'^$', views.ansible_index),
+    url(r'^result/', views.ansible_api)
+]
